f809899f524decb387c8f5a3821f7d10680b8fca
Mohit Varma <varmamohit122@gmail.com>
2022-06-18 02:55:45 +0530

    Updated App.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const urlRouter = require('./routes/urlRoutes');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/', urlRouter);
