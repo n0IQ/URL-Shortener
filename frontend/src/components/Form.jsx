d38efe8ce13f576fb327ba52c6ddfd1defedf658
n0IQ <varmamohit122@gmail.com>
2022-11-06 01:24:07 +0530

    Created Frontend

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+
+function Form() {
+  let [longUrl, setLongUrl] = useState('Shorten your link');
+  let [shortUrl, setShortUrl] = useState('');
+  let [status, setStatus] = useState(0);
+
+  const submitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      const dataObj = await axios.post('http://localhost:8000', { longUrl });
+      // console.log(dataObj);
+
+      const urlObj = dataObj.data;
+
+      setShortUrl((shortUrl = urlObj.data.shortUrl));
+      setStatus((status = dataObj.statusText === 'OK' ? 1 : 0));
+    } catch (err) {
+      console.log(err);
+      alert('Invalid URL');
+    }
+  };
+
+  return (
+    <div className="form">
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={longUrl}
+          onClick={(e) => setLongUrl((longUrl = ''))}
+          onChange={(e) => setLongUrl((longUrl = e.target.value))}
+          className="url-box"
+        ></input>
+        <button type="submit" className="btn">
+          Shorten URL
+        </button>
+        <br />
+        {status === 1 && (
+          <a href={shortUrl} target="_blank" rel="noreferrer">
+            <input
+              type="text"
+              value={shortUrl}
+              className="shortUrl-box"
+            ></input>
+          </a>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default Form;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="title">ShortIT</h1>
+      <p className="summary">
+        A Fast and Simple URL Shortener for transforming long, ugly links into
+        nice, memorable and trackable short URLs.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
